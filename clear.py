c0b0798d1e35be2b4b1f4b12d5ce1724f9223781
Jake Eaton <spud-lord@outlook.com>
2021-03-18 13:25:08 +0000

    clear.py Hotfix
    Important required modules
    Added MacOS and Linux Clear Commands

--- clear.py
+++ clear.py
@@ -1,5 +1,10 @@
 #Jake Eaton
 
+from os import system, name
+
 def clear():
     if name =='nt':
         _ = system('cls')
+
+    else:   #MacOS and Linux Clear Command
+        _ = system('clear')
