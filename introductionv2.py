ee9b6ef4268d39e2b6e669d02487573542e00859
Spud-Lord <spud-lord@outlook.com>
2022-03-05 15:58:20 +0000

    Create introductionv2.py
    New Introduction
    - Improved Storyline

---
+++ introductionv2.py
@@ -0,0 +1,14 @@
+#Jake Eaton
+#Introduction
+
+import time
+import sys
+from os import system, name
+from typing import type, type2
+from clear import clear
+
+def Intro():                            #Defines all indented code as Intro
+    type("You feel yourself being dragged along the floor\n")
+    time.sleep(2)
+    type("")
+    time.sleep(2)
