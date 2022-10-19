e02498ff19dea4998457ae82f61d93d892dcb900
Jake Eaton <spud-lord@outlook.com>
2022-10-19 23:12:10 +0100

    Create victory2.py
    Version 2 of Victory Code

---
+++ victory2.py
@@ -0,0 +1,52 @@
+#Jake Eaton
+#Victory Code
+
+import time
+from typing import type, type2
+import os
+os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
+from pygame import mixer
+
+def Victory():
+    print("")
+    type("Your eyes fly open and you scream in pain as the neural implant is removed from your neck\n")
+    time.sleep(2)
+    type("'Did we get everything?' said a woman's voice\n")
+    time.sleep(2)
+    type("'Yes!' shouted another voice")
+    time.sleep(2)
+    type("You open your eyes to see the armoured woman standing in front of you.")
+    time.sleep(2)
+    type("She removed her helmet and her dark hair was revealed")
+    time.sleep(2)
+    type("You try to lunge at the woman as her identity is finally revealed. 'Emily!' you shout")
+    time.sleep(2)
+    type("Emily smiled at you")
+    time.sleep(2)
+    type("'I know it has been a while.' she spoke calmly. 'As you know, your augmentations were far superior to mine. Now we have the memory, we have what we need to refine your design and create soldiers like you. But better.'")
+    time.sleep(2)
+    type("'So what happens now?' you ask. 'Going to throw me in a cell somewhere?'")
+    time.sleep(2)
+    type("Emily smiled again and moved closer to you")
+    time.sleep(2)
+    type("'No.' she whispers. 'I have something better in store for you.' She took a few steps back.")
+    time.sleep(2)
+    type("Moving like lightning, Emily grabbed her pistol and shot it four times\n")
+    mixer.music.load("4xPistolShot.mp3")
+    mixer.music.play()
+    time.sleep(2)
+    type("Your arms and legs begin to bleed and you yell out in pain\n")
+    time.sleep(2)
+    type("'You'll recognise this method. After all, you taught me it.' she says")
+    time.sleep(2)
+    type("Emily turns and starts to walk away. 'Destroy his ship and leave nothing standing!' she commanded")
+    time.sleep(2)
+    type("You watch Emily walk off as your vision begins to blur\n")
+    time.sleep(2)
+    type("You close your eyes as the pain begins to subside...\n")
+    time.sleep(2)
+    type("Eventually...\n")
+    type2("You...\n")
+    type2("Felt...\n")
+    type2("Nothing...\n")
+    dead = True
